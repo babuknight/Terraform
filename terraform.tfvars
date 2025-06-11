edc14075d78f2522a6e0b90a189b10370ce54c06
Babu Prasath <44191595+babuknight@users.noreply.github.com>
2025-06-11 07:19:34 +0000

    test4

---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+region_name    = "ap-south-1"
+env_name       = "dev"
+vpc_cidr       = "10.0.0.0/16"
+public_cidr    = "10.0.1.0/24"
+private_cidr   = "10.0.2.0/24"
+instance_type  = "t2.micro"
+ami_id         = "ami-0e670eb768a5fc3d4"  # Example AMI for Amazon Linux 2 in ap-south-1
--- variables.tf
+++ variables.tf
@@ -1,13 +1,34 @@
-variable "region" {}
-variable "iam_profile" {}
-variable "vpc_cidr_block" {}
-variable "public_subnet_count" {}
-variable "private_subnet_count" {}
-variable "webserver_launch_template_name" {}
-variable "webserver_ec2_instance_type" {}
-variable "webserver_asg_desired_capacity" {}
-variable "webserver_asg_max_size" {}
-variable "webserver_asg_min_size" {}
-variable "app_port" {}
-variable "webserver_tg_name" {}
-variable "websever_lb_sg_name" {}
+variable "region_name" {
+  description = "AWS region to deploy into"
+  type        = string
+}
+
+variable "env_name" {
+  description = "Environment name (e.g. dev, prod)"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+}
+
+variable "private_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
