3a3d80f4d2549b43b7edd05a3e4fa74c4997a3d2
Babu Prasath <44191595+babuknight@users.noreply.github.com>
2025-06-11 00:13:33 +0000

    last

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "mohan-terraform-tfstate-file"
-    key          = "terraform.tfstate"
-    region       = "ap-south-1"
-    use_lockfile = true
-
-  }
-}
